0d4d4730003fd52ca1a1d0c39af1434dab1399d1
fduncanh <fduncanh@gmail.com>
2021-12-15 23:30:50 -0500

    test  to see if iOS 9 and iOS 10 audio can be supported

--- lib/global.h
+++ lib/global.h
@@ -7,7 +7,7 @@
 
 /* use old protocol  for AES key if User-Agent string is contained in these strings */
 /* replace xxx by any new User-Agent string as needed */
-#define OLD_PROTOCOL_AUDIO_CLIENT_LIST "AirMyPC/2.0;xxx"
+#define OLD_PROTOCOL_AUDIO_CLIENT_LIST "AirMyPC/2.0;AirPlay/260.26;AirPlay/320.20;xxx"
 #define OLD_PROTOCOL_VIDEO_CLIENT_LIST "AirMyPC/2.0;xxx"
 
 #define MAX_HWADDR_LEN 6
