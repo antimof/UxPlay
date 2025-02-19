c864b469812d241f3ae1124d25d37135966e2673
Kim, Hwan <akon47@naver.com>
2025-02-20 08:24:31 +0900

    fix: Fix missing htonll definition when building for Windows

--- lib/byteutils.c
+++ lib/byteutils.c
@@ -31,6 +31,9 @@
 # ifndef ntonll
 #  define ntohll(x) ((1==ntohl(1)) ? (x) : (((uint64_t)ntohl((x) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((x) >> 32)))
 # endif
+#ifndef htonll
+#  define htonll(x) ((1==htonl(1)) ? (x) : (((uint64_t)htonl((x) & 0xFFFFFFFFUL)) << 32) | htonl((uint32_t)((x) >> 32)))
+#endif
 #else
 #  ifndef htonll
 #   ifdef SYS_ENDIAN_H
